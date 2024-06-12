7f828c7739419967eb89c92dd8cda489e5dcde80
S0lenya11 <125654058+hunelii@users.noreply.github.com>
2024-06-12 13:59:58 +0300

    Update fetch_lottery_results.php

--- projects/loto/templates/fetch_lottery_results.php
+++ projects/loto/templates/fetch_lottery_results.php
@@ -1,5 +1,32 @@
 <?php
+
+// URL of the website
 $url = 'https://www.millipiyangoonline.com/sayisal-loto/cekilis-sonuclari.61.2024';
-$data = file_get_contents($url);
-echo $data;
+
+// Create a DOMDocument object
+$dom = new DOMDocument();
+
+// Load the HTML from the URL
+$dom->loadHTMLFile($url);
+
+// Create a DOMXPath object
+$xpath = new DOMXPath($dom);
+
+// Define the XPath query to locate the winning numbers
+$query = "//div[@class='kt-portlet__body']//strong";
+
+// Query the DOM with the XPath expression
+$result = $xpath->query($query);
+
+// Check if there are any results
+if ($result->length > 0) {
+    // Extract and display the winning numbers
+    $winningNumbers = [];
+    foreach ($result as $node) {
+        $winningNumbers[] = $node->nodeValue;
+    }
+    echo "Winning Numbers: " . implode(", ", $winningNumbers);
+} else {
+    echo "Winning numbers not found!";
+}
 ?>
