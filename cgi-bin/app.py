f763f27b60a988bbc490c9a096b188cfaedd3284
S0lenya11 <125654058+hunelii@users.noreply.github.com>
2024-09-04 09:27:58 +0300

    .htaccess added

--- cgi-bin/app.py
+++ cgi-bin/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-x=1
-y=2
-
+x = 1
+y = 2
 
 print("Content-type: text/html\n")
-print("<html><body><h1>Hello, World!</h1></body></html>")
-print(x+y)
+print("<html><body>")
+print("<h1>Hello, World!</h1>")
+print("<p>The sum of x and y is: {}</p>".format(x + y))
+print("</body></html>")
